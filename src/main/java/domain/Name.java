7eaaeb8c6e25b290ac2b8465c779c9b11361e29d
sghwan <anan123456@naver.com>
2022-10-31 17:49:47 +0900

    feat(Name): 자동차의 이ë름이 5자가 넘으면 예외를 발생시킴

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -2,8 +2,12 @@ package domain;
 
 public class Name {
     private final String name;
+    private static final int VALID_NAME_LENGTH = 5;
 
     public Name(String name) {
+        if (name.length() > VALID_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
+        }
         this.name = name;
     }
 }
